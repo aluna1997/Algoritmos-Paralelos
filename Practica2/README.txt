2a06b6d529f2bc58bd251badbae0d43ad0024b8e
aluna1997 <aluna1997@ciencias.unam.mx>
2019-02-15 21:14:34 -0600

    Practica02 sin punto extra terminada

--- Practica2/README.txt
+++ Practica2/README.txt
@@ -1,10 +1,14 @@
 ALGORITMOS PARALELOS
-Práctica01
+Práctica02
 Luna Felipe 313203079
 
 Para compilar el programa hacemos:
- - gcc Practica01.c -o Practica01.out -fopenmp
+ - gcc Practica02.c -o Practica02.out -fopenmp -lm
+donde fopenmp es la biblioteca que usamos para el uso de hilos y
+lm la biblioteca para hacer funciones matemáticas.
 
 Para ejecutar el programa:
- - ./Practica01.out N
-   donde N es el número de procesadores(hilos)
+ - ./Practica02.out 2 N
+   donde N es la potencia del tamaño del arreglo, por ejemplo,
+   si N=2 tendremos un arreglo de numeros aleatorios de longitud
+   2^2=4
--- Practica2/src/Practica02.c
+++ Practica2/src/Practica02.c
@@ -19,38 +19,50 @@ int main(int argc, char** argv){
 		printf("Por favor especifique el exponente para definir el tamaño del arreglo\n");
 		exit(1);
 	}else if(argc == 2){
+		//Tomamos la potencia ingresada en terminal
 		sscanf(argv[1], "%i", &potencia);
+		//El tamaño de el arreglo a ordenar será de 2^(potencia) + 1, esa posicion extra
+		//estará vacia y nos ayudara a no desbordar del arreglo al comparar los elementos impares
 		int tamanio = pow(2,potencia) + 1;
+		//Inicializamos el arreglo
 		int arreglo[tamanio];
+		//La siguiente linea nos ayuda a generar nums aleatorios nuevos en cada
+		//ejecución del programa
 		srand(time(0)); 
+		//Llenamos el arreglo de numeros aleatorios entre 0 y 100
 		for(int i = 1; i < tamanio; i++){
 			arreglo[i] = rand() % 101;
 		}
-
+		//Imprimimos en pantalla el arreglo original
 		printf("Arreglo inicial: \n");
-		for (int i = 0; i < tamanio; i++){
+		for (int i = 1; i < tamanio; i++){
 			printf("%i ",arreglo[i]);		
 		}
 		printf("\n");
-		
+		//El numero de procesadores(hilos) a utilizar sera la longitud del arreglo,
+		//es decir,un procesador por elemento
 		omp_set_num_threads(tamanio);
 		//Inicia seccion paralela
 		#pragma omp parallel
-		{
+		{	
+			//Contador que se ejecutara tamanio / 2 veces
 			int i = 0;
+			//Apuntador a los elementos pares del arreglo
 			int par = 0;
+			//Apuntador a los elementos impares del arreglo
 			int impar = 1;
 			do{
-				
-				
+				//Si el elemento actual es mayor al siguiente,
+				//lo intercambiamos, esto para las posiciones pares
 				if(arreglo[par + 1] < arreglo[par]){
 					int aux = arreglo[par];
 					arreglo[par] = arreglo[par + 1];
 					arreglo[par + 1] = aux;
 				}
 				par += 2;
-
 				
+				//Si el elemento actual es mayor al siguiente,
+				//lo intercambiamos, esto para las posiciones impares
 				if(arreglo[impar + 1] < arreglo[impar]){
 					int aux = arreglo[impar];
 					arreglo[impar] = arreglo[impar + 1];
@@ -58,15 +70,13 @@ int main(int argc, char** argv){
 				}
 
 				impar += 2;
-				
 				i +=1 ;
 					
 			}while(i < (tamanio / 2));
-		}
-		
-		//Termina seccion paralela
+		}//Termina seccion paralela
+		//Imprimimos en pantalla el arreglo ordenado
 		printf("Arreglo ordenado: \n");
-		for (int i = 0; i < tamanio; i++){
+		for (int i = 1; i < tamanio; i++){
 			printf("%i ",arreglo[i]);		
 		}
 		printf("\n");
@@ -75,4 +85,3 @@ int main(int argc, char** argv){
 	}
 
 }
-
